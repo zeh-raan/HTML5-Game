bf8c243d28db1a7937cc01e8588891ff0de8ba95
dotping-me <dotping.hpg@gmail.com>
2026-03-04 14:16:11 +0400

    Hello World

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// This is the project's entry point
+// Script loaded with defer, so document body should be all set
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
